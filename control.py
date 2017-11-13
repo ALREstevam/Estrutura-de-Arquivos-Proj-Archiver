fa3f5acbe3921e78413dcea32cb7073f234aa00b
KaulitzGuimaraes <kaulitzguimaraes@hotmail.com>
2017-11-13 16:09:08 -0200

    method insert from control

--- control.py
+++ control.py
@@ -6,21 +6,25 @@ class Control(object) :
     def list_files(self) :
         pass
     def insert_files(self) :
-        pass
+        print("Insert the directory path :")
+        dir_path = input()
+        print ( "Insert the file name :" )
+        file_name = input()
+        new_file = filesAr.import_file(dir_path,file_name)
+
     def remove_files(self):
         pass
     def __init__(self):
         lis = ["Quit","List files","Insert file","Remove Files"]
-
-        #print ( choices.values () )
+s
         self.menuInit = menu.Menu(lis)
         op = self.menuInit.start_menu()
 
-        if(op == 0) :
+        if op == 0 :
             exit()
-        elif(op == 1) :
+        elif op == 1 :
             self.list_files()
-        elif(op == 2):
+        elif op == 2:
             self.insert_files()
         else :
             self.remove_files()
