c7ee2813f6f4ac187d19db93935fe92e98be0db1
KaulitzGuimaraes <kaulitzguimaraes@hotmail.com>
2017-11-14 14:44:08 -0200

    class control

--- control.py
+++ control.py
@@ -3,20 +3,10 @@ import menu
 import  filesAr
 
 class Control(object) :
-    def list_files(self) :
-        pass
-    def insert_files(self) :
-        print("Insert the directory path :")
-        dir_path = input()
-        print ( "Insert the file name :" )
-        file_name = input()
-        new_file = filesAr.import_file(dir_path,file_name)
 
-    def remove_files(self):
-        pass
     def __init__(self):
-        lis = ["Quit","List files","Insert file","Remove Files"]
-s
+        lis = ["Quit","List files","Insert file","Remove Files","Extract File"]
+
         self.menuInit = menu.Menu(lis)
         op = self.menuInit.start_menu()
 
@@ -26,9 +16,26 @@ s
             self.list_files()
         elif op == 2:
             self.insert_files()
-        else :
+        elif op == 3 :
             self.remove_files()
+        else :
+            self.extract_files()
+
+    def list_files(self):
+        pass
+
+    def insert_files(self):
+        print ( "Insert the directory path :" )
+        dir_path = input ()
+        print ( "Insert the file name :" )
+        file_name = input ()
 
 
+    def remove_files(self):
+        pass
 
-
+    def extract_files(self):
+        print ( "Insert the directory path :" )
+        dir_path = input ()
+        print ( "Insert the file name :" )
+        file_name = input ()
