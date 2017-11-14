25efae33365ff0a07764fd5af221ffa70f68e345
KaulitzGuimaraes <kaulitzguimaraes@hotmail.com>
2017-11-14 15:10:10 -0200

    class files ok

--- filesAr.py
+++ filesAr.py
@@ -2,14 +2,15 @@ import os
 
 def import_file (directory,name_file) :
    os.chdir(directory)
-   new_file = open(name_file,"rb")
-   return  name_file
+   new_file = open(name_file,"r+b")
+   return  new_file
 
 def export_file (directory,file) :
-    read_file = open( file , 'rb' )
+
+    read_file = open( file , 'r+b' )
     read_file = read_file.read()
     os.chdir (directory)
-    new_file = open ( file , "wb" )
+    new_file = open ( file, "w+b" )
     new_file.write(read_file)
 
 
