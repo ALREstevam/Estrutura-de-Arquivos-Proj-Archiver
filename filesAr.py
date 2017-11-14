5e26f5eb82328055c2c6ce4faa946edfb43d9512
KaulitzGuimaraes <kaulitzguimaraes@hotmail.com>
2017-11-14 20:49:55 -0200

    class file working

--- filesAr.py
+++ filesAr.py
@@ -3,16 +3,24 @@ import os
 def import_file (directory,name_file) :
    os.chdir(directory)
    new_file = open(name_file,"r+b")
+
+
    return  new_file
 
 def export_file (directory,file) :
-
-    read_file = open( file , 'r+b' )
-    read_file = read_file.read()
+    name = file.name
+    read_file = file.read()
     os.chdir (directory)
-    new_file = open ( file, "w+b" )
-    new_file.write(read_file)
+    new_file = open ( name, "w+b" )
+    file.write(read_file)
 
 
 
 
+
+dir = input()
+nfile = input()
+file = import_file(dir,nfile)
+print(file)
+dir = input()
+export_file(dir,file)
